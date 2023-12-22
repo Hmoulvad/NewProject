011c0b3705f33ac6c47154015881ac321e8d42b8
Hannibal B. Moulvad <IMPACT A/S>
2023-12-22 10:06:37 +0100

    Remove unused imports and state variable from Modal component

--- src/components/UI/Modal/index.tsx
+++ src/components/UI/Modal/index.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import React, { forwardRef, useEffect, useImperativeHandle } from "react";
+import React, { forwardRef, useImperativeHandle } from "react";
 import styles from "./styles.module.css";
-import clsx from "clsx";
 
 type Props = {
   children: React.ReactNode;
@@ -17,7 +16,6 @@ const Modal = forwardRef<ModalOperations, Props>(function ModalRef(
   { children },
   ref
 ) {
-  const [isClosing, setIsClosing] = React.useState(false);
   const dialogRef = React.useRef<HTMLDialogElement>(null);
 
   useImperativeHandle(
