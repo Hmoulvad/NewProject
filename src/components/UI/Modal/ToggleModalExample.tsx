e8ce5ae99f4efc07c7307b949d91a5e0bcde22f3
Hannibal B. Moulvad <IMPACT A/S>
2023-12-22 21:57:03 +0100

    Refactor modal references and add new modals

--- src/components/UI/Modal/ToggleModalExample.tsx
+++ src/components/UI/Modal/ToggleModalExample.tsx
@@ -6,14 +6,29 @@ import Button from "../Button";
 import Typography from "../Typography";
 
 export default function ToggleModalExample() {
-  const modalRef = React.useRef<ModalOperations>(null);
+  const sidebarModalRef = React.useRef<ModalOperations>(null);
+  const centerModalRef = React.useRef<ModalOperations>(null);
 
   return (
     <>
-      <Button variant="secondary" onClick={() => modalRef.current?.open()}>
-        Open Modal
+      <Button
+        variant="secondary"
+        onClick={() => sidebarModalRef.current?.open()}
+      >
+        Open Sidebar Modal
       </Button>
-      <Modal variant="sidebar" ref={modalRef}>
+      <Modal variant="sidebar" ref={sidebarModalRef}>
+        <Typography variant="display4" inverted>
+          This is some content of the Modal
+        </Typography>
+      </Modal>
+      <Button
+        variant="secondary"
+        onClick={() => centerModalRef.current?.open()}
+      >
+        Open Center Modal
+      </Button>
+      <Modal variant="center" ref={centerModalRef}>
         <Typography variant="display4" inverted>
           This is some content of the Modal
         </Typography>
