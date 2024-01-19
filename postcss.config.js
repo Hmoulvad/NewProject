0adcabd779c160f8280ced3c405de79ae1fea690
Hannibal B. Moulvad <IMPACT A/S>
2024-01-19 22:27:54 +0100

    Add custom media queries and postcss-global-data

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,18 @@
+const customMediaImports = 'src/styles/media.css';
+
 module.exports = {
   plugins: {
     "postcss-flexbugs-fixes": {},
+    '@csstools/postcss-global-data': {
+      files: [customMediaImports],
+    },
     "postcss-preset-env": {
       autoprefixer: {
         flexbox: "no-2009",
       },
       stage: 3,
       features: {
+        'custom-media-queries': true,
         "custom-properties": false,
         "nesting-rules": true,
       },
