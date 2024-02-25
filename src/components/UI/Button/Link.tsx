7376aadbf86912e41aa658d2bd81651cc9117a66
Hannibal B. Moulvad <IMPACT A/S>
2024-02-25 19:28:07 +0100

    Fix import in Link component

--- src/components/UI/Button/Link.tsx
+++ src/components/UI/Button/Link.tsx
@@ -1,9 +1,9 @@
 import clsx from "clsx";
 import NextLink, { LinkProps } from "next/link";
-import Typography from "../Typography";
 import { getButtonSizeStyle, getButtonVariantStyle } from "./getButtonStyles";
 import styles from "./styles.module.css";
 import { ButtonProps } from "./types";
+import { Typography } from "../Typography";
 
 type Props = ButtonProps &
   LinkProps &
