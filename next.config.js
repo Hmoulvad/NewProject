b37795549d7311b4b542baaa5a67f844c2a0a174
Hannibal B. Moulvad <hbm@impact.dk>
2024-03-19 14:06:37 +0100

    Add PWA support and manifest file

--- next.config.js
+++ next.config.js
@@ -4,7 +4,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
 }
 
-module.exports = withBundleAnalyzer(nextConfig)
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+})
+
+module.exports = withBundleAnalyzer(withPWA(nextConfig))
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,35 @@ import { Footer } from "@/features/Navigation/Footer";
 import { Header } from "@/features/Navigation/Header";
 import Scripts from "@/features/Scripts";
 import "../styles/globals.css";
+import type { Metadata, Viewport } from "next";
+
+export const metadata: Metadata = {
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  creator: "Hannibal B. Moulvad",
+  keywords: [
+    "next.js",
+    "react",
+    "typescript",
+    "next14",
+    "nextjs14",
+    "next-pwa",
+    "pwa",
+  ],
+  authors: [{ name: "Hannibal B. Moulvad" }],
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
+};
+
+export const viewport: Viewport = {
+  minimumScale: 1,
+  initialScale: 1,
+  width: "device-width",
+  viewportFit: "cover",
+  userScalable: false,
+};
 
 export default function RootLayout({
   children,
