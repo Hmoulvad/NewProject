943655e4ad71eb7fdfae39fd8bb1565fb4662b85
Hannibal B. Moulvad <hbm@impact.dk>
2024-06-13 17:18:04 +0200

    chore: Update npm dependencies and refactor UI imports

--- src/components/UI/Drawer/types.ts
+++
@@ -1,6 +0,0 @@
-import { ComponentProps } from 'react';
-import AnimatePresence from 'services/ui/lib/AnimatePresence';
-
-export type DrawerVariant = 'default' | 'mobile-mega-menu';
-
-export type DrawerAnimations = ComponentProps<typeof AnimatePresence>['animation'];
